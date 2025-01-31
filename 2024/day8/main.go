3827163a3c90a4be37425388872f7c2cb4379ec5
Treyson <treyson.grange@outlook.com>
2025-01-31 14:35:16 -0700

    day 8, idk how much is done, wiping device

---
+++ 2024/day8/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"aoc2024/utils"
+	"fmt"
+)
+
+func main() {
+	lines, err := utils.ReadFileLines("./input.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	fmt.Println(lines)
+}
